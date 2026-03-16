aa86dc340c18f428968fe12a28062e5b236691e5
HateCrewEphex <abraham.salazarmir98@gmail.com>
2026-03-16 12:54:23 -0600

    Add files via upload

---
+++ PerfectStrangers/src/main/java/com/mycompany/perfectstrangers/PerfectStrangers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perfectstrangers;
+
+/**
+ *
+ * @author Ephex
+ */
+public class PerfectStrangers {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
